8d580e8d91df424b035535e53eba206b18ce03a4
gbjbuobbl <79749963+gbjbuobbl@users.noreply.github.com>
2021-09-03 14:37:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup() {
+    createCanvas(300,300);
+    Canvas.center();
+}
+function draw() {
+
+}
+function take_snapshot() {
+    save(filteredimage.jpeg);
+}
